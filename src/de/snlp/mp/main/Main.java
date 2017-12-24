3328ab6ea3a1cf4966658c7d3682dfa1a684b790
Daniel Possienke <Daniel.Possienke@googlemail.com>
2017-12-24 14:41:16 +0100

    Add random article selection and a file which contains the names of all processed aritcles

--- src/de/snlp/mp/main/Main.java
+++ src/de/snlp/mp/main/Main.java
@@ -1,11 +1,15 @@
 package de.snlp.mp.main;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
@@ -17,25 +21,66 @@ import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 
 public class Main extends StanfordCoreNLP {
 
-	private static final String corpus = "Wikipedia Corpus";
+	private static String corpusName = "Wikipedia Corpus";
+	private static File corpusFolder;
+
+	private static List<String> processedDocsList = new ArrayList<String>();
+	private static final File processedDocsFile = new File("processedDocs.txt");
 
 	private static StanfordCoreNLP pipeline;
+
+	private static int fileCounter;
 
 	/*
 	 * Wichtige Links: https://stanfordnlp.github.io/CoreNLP/ http://universaldependencies.org/introduction.html
 	 */
 
 	public static void main(String[] args) throws IOException {
-		File folder = new File(corpus);
-		if (!folder.exists()) {
-			System.out.println("Cannot find the folder: " + folder.getAbsolutePath());
+		if (args.length == 0) {
+			System.out.println("No argument found. Use \"Wikipedia Corpus\" as corpus folder.");
+		} else {
+			corpusName = args[1];
+		}
+		corpusFolder = new File(corpusName);
+		if (!corpusFolder.exists()) {
+			System.out.println("Cannot find the folder: " + corpusFolder.getAbsolutePath());
 			return;
 		}
-		donwloadModel();
+
 		initStandFordLib();
-		goThroughCorpus(folder);
+		setFileCount(corpusFolder);
+		if (!processedDocsFile.exists())
+			processedDocsFile.createNewFile();
+		readProcessedDocs();
+		// Laufe solange, bis die Anzahl der bearbeiteten Artikel der Anzahl der .txt-Dateien im Korpus entspricht
+		while (fileCounter > processedDocsList.size()) {
+			File f = getRawRandomFile(corpusFolder);
+			readFile(f);
+			processedDocsList.add(f.getName());
+			addProcessedDoc(f.getName());
+		}
+
 	}
 
+	/**
+	 * Initialisiert die StanfordCoreNLP Bibliothek. Falls das Modell für die englische Sprache fehlt wird es das Programm beendet und eine
+	 * entsprechende Nachricht ausgegeben.
+	 */
+	private static void initStandFordLib() {
+		try {
+			Properties props = new Properties();
+			props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref, relation");
+			pipeline = new StanfordCoreNLP(props);
+		} catch (RuntimeIOException e) {
+			System.out.println("Can't find the english language model file.");
+			donwloadModel();
+			System.exit(255);
+		}
+	}
+
+	/**
+	 * Lädt das Modell für die englische Sprache herunter und beendet das Programm falls ein Fehler beim Download auftritt.
+	 */
 	private static void donwloadModel() {
 		File file = new File("stanford-english-corenlp-models.jar");
 		try {
@@ -43,9 +88,9 @@ public class Main extends StanfordCoreNLP {
 				System.out.println("Download the model file for the english language.");
 				URL url = new URL("https://nlp.stanford.edu/software/stanford-english-corenlp-2017-06-09-models.jar");
 				FileUtils.copyURLToFile(url, file);
-				System.out.println("Download finish. Add the .jar file to the build path.");
-				System.exit(1);
 			}
+			System.out.println(
+					"Download finish. Add the \\\"stanford-english-corenlp-models.jar\\\" file to the build path and/or refresh the project.");
 		} catch (IOException e) {
 			System.out.println("Download failed. Exit program.");
 			if (file.exists())
@@ -55,27 +100,70 @@ public class Main extends StanfordCoreNLP {
 		}
 	}
 
-	private static void initStandFordLib() {
-		try {
-			Properties props = new Properties();
-			props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref, relation");
-			pipeline = new StanfordCoreNLP(props);
-		} catch (RuntimeIOException e) {
-			System.out.println("Can't find the model file. Add the \"stanford-english-corenlp-models.jar\" file to the build path and refresh the project.");
-			System.exit(255);
+	/**
+	 * Lädt die Datei, in der die Namen der bereits abgearbeiteten Artikel drin stehen und fügt die zur Liste 'processedDocsFile' hinzu.
+	 */
+	private static void readProcessedDocs() {
+		try (BufferedReader reader = new BufferedReader(new FileReader(processedDocsFile))) {
+			String line = "";
+			while ((line = reader.readLine()) != null) {
+				processedDocsList.add(line);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
-	private static void goThroughCorpus(File folder) {
+	/**
+	 * Fügt eine neuen Namen zur Liste der bereits abgearbeiteten Artikel hinzu.
+	 * @param Name des neuen abgearbeiteten Artikels
+	 */
+	private static void addProcessedDoc(String name) {
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(processedDocsFile, true))) {
+			writer.write(name + "\n");
+			writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Zählt die Anzahl der .txt-Dateien. Die Anzahl wird gebraucht, damit das Programm weiß, wann es mit der Bearbeitung fertig ist, da die
+	 * Artikel zufällig und nicht chronologisch bearbeitet werden.
+	 * @param Der Ordner mit dem Korpus
+	 */
+	private static void setFileCount(File folder) {
 		for (File f : folder.listFiles()) {
-			if (f.isDirectory()) {
-				goThroughCorpus(f);
-			} else {
-				readFile(f);
+			if (f.isDirectory())
+				setFileCount(f);
+			else if (f.getName().endsWith(".txt")) {
+				fileCounter++;
 			}
 		}
 	}
 
+	/**
+	 * Sucht rekursiv zufällig einen Artikel heraus, der noch nicht bearbeitet wurde, also noch nicht in der Liste vorhanden ist.
+	 * @param Der Ordner mit dem Korpus
+	 * @return Unbearbeiteter Artikel
+	 */
+	private static File getRawRandomFile(File folder) {
+		int rnd = (int) (Math.random() * folder.listFiles().length);
+		if (folder.listFiles()[rnd].isDirectory()) {
+			return getRawRandomFile(folder.listFiles()[rnd]);
+		} else {
+			if (processedDocsList.contains(folder.listFiles()[rnd].getName())) {
+				return getRawRandomFile(corpusFolder);
+			} else {
+				return folder.listFiles()[rnd];
+			}
+		}
+	}
+
+	/**
+	 * Hier soll der Artikel verarbeitet werden. Bis jetzt wird nur die XML Datei gespeichert.
+	 * @param f
+	 */
 	private static void readFile(File f) {
 		try (BufferedReader reader = new BufferedReader(new FileReader(f))) {
 			String line = "";
@@ -89,6 +177,12 @@ public class Main extends StanfordCoreNLP {
 		}
 	}
 
+	/**
+	 * Siehe Methode readFile
+	 * @param f
+	 * @param content
+	 * @throws IOException
+	 */
 	private static void createXMLFile(File f, String content) throws IOException {
 		PrintWriter xmlOut = new PrintWriter(f.getName().replaceAll(".txt", ".xml"));
 		Annotation annotation = new Annotation(content);
