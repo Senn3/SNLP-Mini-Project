664fb1f3be1b4e7efebc8627d48832ab6a2121f1
Patrick Thiele <thielep@mail.uni-paderborn.de>
2018-01-17 16:12:36 +0100

    Updated FactChecker

--- src/de/snlp/mp/fact_checking/FactChecker.java
+++ src/de/snlp/mp/fact_checking/FactChecker.java
@@ -1,12 +1,19 @@
 package de.snlp.mp.fact_checking;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
+import de.snlp.mp.text_analysis.StanfordLib;
+import de.snlp.mp.text_model.Corefs;
+import de.snlp.mp.text_model.TextModel;
+import de.snlp.mp.text_model.Token;
 import edu.mit.jwi.item.POS;
 
 public class FactChecker {
@@ -16,20 +23,24 @@ public class FactChecker {
 	 */
 	private static final String statementFile = "Wikipedia Corpus Cutted/train.txt";
 
+	private static final File pathToFactRelatedTexts = new File("FactRelatedTexts");
+
+	private static final StanfordLib stanfordLib = new StanfordLib();
+
 	/*
 	 * List that contains all verbs from the statement
 	 */
-	private static List<String> verbs = new ArrayList<String>();
-
-	/*
-	 * List that contains all nouns from the statement
-	 */
-	private static List<String> nouns = new ArrayList<String>();
-
-	/*
-	 * List that contains all names from the statement
-	 */
-	private static List<String> names = new ArrayList<String>();
+	// private static List<String> verbs = new ArrayList<String>();
+	//
+	// /*
+	// * List that contains all nouns from the statement
+	// */
+	// private static List<String> nouns = new ArrayList<String>();
+	//
+	// /*
+	// * List that contains all names from the statement
+	// */
+	// private static List<String> names = new ArrayList<String>();
 
 	/*
 	 * List that contains all the words (nouns, names and verbs), that are part of the statement or a synonym of that, and which have been found
@@ -37,40 +48,74 @@ public class FactChecker {
 	 */
 	private static List<String> usedWords = new ArrayList<String>();
 
+	private static SynonymDictionary synonymDictionary = new SynonymDictionary();
+
 	public static void main(String[] args) {
 
 		// TODO don't use stems of words to compare with text
-		List<List<String>> nounsWithSynonyms = getSynonyms(nouns, POS.NOUN);
-		List<List<String>> verbsWithSynonyms = getSynonyms(verbs, POS.VERB);
-
-		System.out.println(nounsWithSynonyms);
-		System.out.println(verbsWithSynonyms);
+		// List<List<String>> nounsWithSynonyms = getSynonyms(nouns, POS.NOUN);
+		// List<List<String>> verbsWithSynonyms = getSynonyms(verbs, POS.VERB);
+		//
+		// System.out.println(nounsWithSynonyms);
+		// System.out.println(verbsWithSynonyms);
 
 		// TODO use actual wikipedia articles here
-		String wikiFile = "angela.txt";
-
-		List<List<String>> wordsWithSynonyms = new ArrayList<List<String>>();
-		wordsWithSynonyms.addAll(nounsWithSynonyms);
-		wordsWithSynonyms.addAll(verbsWithSynonyms);
+		// String wikiFile = "angela.txt";
+		//
+		// List<List<String>> wordsWithSynonyms = new ArrayList<List<String>>();
+		// wordsWithSynonyms.addAll(nounsWithSynonyms);
+		// wordsWithSynonyms.addAll(verbsWithSynonyms);
 		// System.out.println(isStatementAsAWholeInFileUsingSynonyms(statement.toLowerCase(), wikiFile, wordsWithSynonyms, nounsWithSynonyms.get(0),
 		// nounsWithSynonyms.size() + verbsWithSynonyms.size()));
 
-		for (String s : names) {
-			List<List<String>> list = new ArrayList<List<String>>();
-			list.add(new ArrayList<String>());
-			list.get(0).add(s);
-			wordsWithSynonyms.addAll(list);
+		// for (String s : names) {
+		// List<List<String>> list = new ArrayList<List<String>>();
+		// list.add(new ArrayList<String>());
+		// list.get(0).add(s);
+		// wordsWithSynonyms.addAll(list);
+		// }
+		// System.out.println("Test: "
+		// + getLineOfWordsOfStatementInFileUsingSynonyms(wordsWithSynonyms, wordsWithSynonyms.get(0), "", wikiFile, wordsWithSynonyms.size(),
+		// wordsWithSynonyms.size()));
+		// System.out.println(usedWords);
+
+		// String test = "Angela Merkel is working as the chancellor of Germany and inhabit in a topographic place. We need to fill this string with
+		// some nonesense in order to get better results.";
+		// System.out.println(
+		// "Bool: " + areElementsOfListInStringUsingSynonyms(wordsWithSynonyms, wordsWithSynonyms.get(0), "", test, wordsWithSynonyms.size(),
+		// wordsWithSynonyms.size()));
+
+		stanfordLib.initStandFordLib();
+
+		List<Fact> facts = FactFileHandler.readFactsFromFile();
+
+		for (Fact f : facts) {
+			String factStatement = f.getFactStatement();
+			List<String> nouns = getNounsFromTextModel(stanfordLib.getTextModel(factStatement), factStatement);
+			List<String> verbs = getVerbsFromTextModel(stanfordLib.getTextModel(factStatement), factStatement);
+			System.out.println(nouns);
+			System.out.println(verbs);
+
+			List<List<String>> synonyms = getSynonyms(nouns, POS.NOUN);
+			synonyms.addAll(getSynonyms(verbs, POS.VERB));
+
+			// files that contain the same words.
+			File[] relatedFactFiles = getRelatedFactFiles(f.getFactId());
+
+			if (relatedFactFiles != null) {
+				List<String> matchingLines = new ArrayList<String>();
+				for (File fi : getRelatedFactFiles(f.getFactId())) {
+					matchingLines.addAll(getMatchingLinesOfFile(fi, synonyms));
+				}
+				System.out.println(matchingLines);
+			} else
+				f.setTruthvalue(-1.0);
 		}
-		System.out.println("Test: "
-				+ getLineOfWordsOfStatementInFileUsingSynonyms(wordsWithSynonyms, wordsWithSynonyms.get(0), "", wikiFile, wordsWithSynonyms.size(), wordsWithSynonyms.size()));
-		System.out.println(usedWords);
-		
-		String test = "Angela Merkel is working as the chancellor of Germany and inhabit in a topographic place. We need to fill this string with some nonesense in order to get better results.";
-		System.out.println("Bool: " + areElementsOfListInStringUsingSynonyms(wordsWithSynonyms, wordsWithSynonyms.get(0), "", test, wordsWithSynonyms.size(), wordsWithSynonyms.size()));
+
+		FactFileHandler.writeFactsToFile(facts);
 	}
 
 	private static List<List<String>> getSynonyms(List<String> words, POS type) {
-		SynonymDictionary synonymDictionary = new SynonymDictionary();
 		List<List<String>> wordsWithSynonyms = new ArrayList<List<String>>();
 
 		for (int i = 0; i < words.size(); i++) {
@@ -78,46 +123,12 @@ public class FactChecker {
 
 			for (String st : synonymDictionary.getSynonyms(words.get(i), type))
 				wordsWithSynonyms.get(i).add(st.replaceAll("[^a-zA-Z]", " "));
-			System.out.println();
+
+			if (wordsWithSynonyms.get(i).isEmpty())
+				wordsWithSynonyms.get(i).add(words.get(i));
 		}
 
 		return wordsWithSynonyms;
-	}
-
-	private static String getStatement() {
-		try {
-			BufferedReader in = new BufferedReader(new FileReader(statementFile));
-			String line = "";
-			if ((line = in.readLine()) != null) {
-				in.close();
-				return line;
-			}
-			in.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return "";
-	}
-
-	private static boolean isStatementAsAWholeInFile(String statement, String fileName) {
-		try {
-			BufferedReader in = new BufferedReader(new FileReader(fileName));
-			String line = "";
-			while ((line = in.readLine()) != null) {
-				if (line.toLowerCase().contains(statement.toLowerCase())) {
-					in.close();
-					return true;
-				}
-			}
-			in.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return false;
 	}
 
 	private static String getLineOfWordsOfStatementInFileUsingSynonyms(List<List<String>> wordsWithSynonyms, List<String> words, String wordsToCheck, String fileName, int level,
@@ -192,16 +203,23 @@ public class FactChecker {
 
 	/**
 	 * Checks whether all words of a given list or replaced by any of their synonyms are contained inside of a given string.
-	 *  
-	 * @param wordsWithSynonyms A list of lists of words, which shall be found within the string.
-	 * @param words Has to be the first list of words of wordsWithSynonyms.
-	 * @param wordsToCheck Contains the words that are checked in the current recursion. Has to be an empty string "".
-	 * @param text The text to look in.
-	 * @param level The level of the current recursion. Start at maxLevel.
-	 * @param maxLevel The maximum level of the recursion. Has to be the size of wordsWithSynonyms.
+	 * 
+	 * @param wordsWithSynonyms
+	 *            A list of lists of words, which shall be found within the string.
+	 * @param words
+	 *            Has to be the first list of words of wordsWithSynonyms.
+	 * @param wordsToCheck
+	 *            Contains the words that are checked in the current recursion. Has to be an empty string "".
+	 * @param text
+	 *            The text to look in.
+	 * @param level
+	 *            The level of the current recursion. Start at maxLevel.
+	 * @param maxLevel
+	 *            The maximum level of the recursion. Has to be the size of wordsWithSynonyms.
 	 * @return
 	 */
-	public static boolean areElementsOfListInStringUsingSynonyms(List<List<String>> wordsWithSynonyms, List<String> words, String wordsToCheck, String text, int level, int maxLevel) {
+	public static boolean areElementsOfListInStringUsingSynonyms(List<List<String>> wordsWithSynonyms, List<String> words, String wordsToCheck, String text, int level,
+			int maxLevel) {
 		if (level == 0) {
 			return areWordsInText(wordsToCheck, text);
 		} else {
@@ -216,8 +234,8 @@ public class FactChecker {
 
 				boolean isInText = false;
 				if (level != 1) {
-					isInText = areElementsOfListInStringUsingSynonyms(wordsWithSynonyms, wordsWithSynonyms.get(wordsWithSynonyms.size() - level + 1), wordsToCheck, text,
-							level - 1, maxLevel);
+					isInText = areElementsOfListInStringUsingSynonyms(wordsWithSynonyms, wordsWithSynonyms.get(wordsWithSynonyms.size() - level + 1), wordsToCheck, text, level - 1,
+							maxLevel);
 					if (isInText)
 						return true;
 				} else {
@@ -228,6 +246,40 @@ public class FactChecker {
 			}
 		}
 		return false;
+	}
+
+	private static List<String> getNounsFromTextModel(TextModel model, String factStatement) {
+		List<String> nouns = new ArrayList<String>();
+		for (Corefs c : model.getCorefsList()) {
+			if (c.getType().equals("PROPER")) {
+				if (c.getText().contains(" , ")) {
+					nouns.add(c.getText().split(" , ")[0]);
+					nouns.add(c.getText().split(" , ")[1]);
+				} else {
+					nouns.add(c.getText().replaceAll(" 's", ""));
+				}
+			}
+		}
+
+		for (Token t : model.getSentences().get(0).getTokens()) {
+			if (t.getPos().contains("NN") && !t.getPos().contains("P")) {
+				nouns.add(t.getOriginalText());
+			}
+
+		}
+		return nouns;
+	}
+
+	private static List<String> getVerbsFromTextModel(TextModel model, String factStatement) {
+		List<String> verbs = new ArrayList<String>();
+		for (Token t : model.getSentences().get(0).getTokens()) {
+			if (t.getPos().contains("VB") || t.getPos().contains("VBD") || t.getPos().contains("VBG") || t.getPos().contains("VBZ") || t.getPos().contains("VBN")
+					|| t.getPos().contains("VBP")) {
+				verbs.add(t.getOriginalText());
+			}
+
+		}
+		return verbs;
 	}
 
 	private static boolean areWordsInText(String wordsToCheck, String text) {
@@ -245,5 +297,45 @@ public class FactChecker {
 		if (contained == true)
 			return true;
 		return false;
+	}
+
+	private static File[] getRelatedFactFiles(int id) {
+		File relatedFactFilePath = new File(pathToFactRelatedTexts, String.valueOf(id));
+
+		if (!relatedFactFilePath.exists())
+			return null;
+
+		return relatedFactFilePath.listFiles();
+	}
+
+	private static List<String> getMatchingLinesOfFile(File f, List<List<String>> synonyms) {
+		boolean[] eachWordInList = new boolean[synonyms.size()];
+		List<String> lines = new ArrayList<String>();
+		try (BufferedReader reader = new BufferedReader(new FileReader(f))) {
+			String line = "";
+			while ((line = reader.readLine()) != null) {
+				Arrays.fill(eachWordInList, false);
+				
+				for (int i = 0; i < eachWordInList.length; i++) {
+					for (String s : synonyms.get(i)) {
+						if (line.contains(s))
+							eachWordInList[i] = true;
+					}
+				}
+				
+				if (arrayIsTrue(eachWordInList))
+					lines.add(line);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return lines;
+	}
+
+	private static boolean arrayIsTrue(boolean[] a) {
+		for (boolean value : a)
+			if (!value)
+				return false;
+		return true;
 	}
 }
